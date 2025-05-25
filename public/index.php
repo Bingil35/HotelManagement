3d57c29eaf3ee536f3e298084338a9a0e495aa5f
Bingil35 <nguyenvanbinh352005@gmail.com>
2025-05-25 19:38:55 +0700

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/base.css">
+    <link rel="stylesheet" href="./css/auth.css">
+</head>
+<body>
+    <div class="container">
+        <div class="auth-wrapper">
+            <form class="form-login" action="" method="post">
+                <h2>Đăng nhập</h2>
+                <label for="username">Tên tài khoản</label>
+                <input type="email" name="username" id="" required>
+
+                <label for="password">Mật khẩu</label>
+                <input type="password" name="password" id="" required>
+
+                <a href="forgot_password.php">Quên mật khẩu?</a>
+                <button type="submit" value="login">Đăng nhập</button>
+            </form>
+        </div>
+
+        <div class="auth-image">
+
+        </div>
+    </div>
+</body>
+</html>
